0ba61c80276f6e1d59d405a48b4e74428ee0a1f1
Nicholas Smith <nick.smith@cern.ch>
2025-04-15 17:43:45 -0500

    Only stat file on GET if needed (#44)
    Fixes #42

--- nginx/lua/webdav_read_header_filter.lua
+++ nginx/lua/webdav_read_header_filter.lua
@@ -1,11 +1,8 @@
 local ngx = require("ngx")
 local fileutil = require("fileutil")
 
-
-local path = fileutil.get_request_local_path()
-local want_adler32 = ngx.var.http_want_digest == "adler32"
-local stat = fileutil.get_metadata(path, want_adler32)
-
-if want_adler32 then
+if ngx.var.http_want_digest == "adler32" then
+  local path = fileutil.get_request_local_path()
+  local stat = fileutil.get_metadata(path, true)
   ngx.header["Digest"] = "adler32=" .. stat.adler32
 end
