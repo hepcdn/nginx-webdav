f9ee1438bb6788317712d5e75dfd38a71191c946
Nick Smith <nick.smith@cern.ch>
2025-04-16 21:59:30 -0500

    No need for separate HEAD handler

--- nginx/lua/webdav_read_header_filter.lua
+++ nginx/lua/webdav_read_header_filter.lua
@@ -1,7 +1,7 @@
 local ngx = require("ngx")
 local fileutil = require("fileutil")
 
-if ngx.var.http_want_digest == "adler32" then
+if ngx.status == ngx.HTTP_OK and ngx.var.http_want_digest == "adler32" then
   local path = fileutil.get_request_local_path()
   local stat = fileutil.get_metadata(path, true)
   ngx.header["Digest"] = "adler32=" .. stat.adler32
